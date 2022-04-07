d24f76f8451274dce933ffcdda5e3a181f5a17a9
JC-Tarb <101950168+JC-Tarb@users.noreply.github.com>
2022-04-07 16:56:01 -0400

    html change

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next.js Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js app appplication In development. Continuous improvement from my github repository. Changes being pushed every day!" />
+        <Header title="Next.js Web App In Development. Continuous improvement from my github repository. Changes being pushed every day!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
